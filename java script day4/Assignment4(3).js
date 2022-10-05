c32d1864ca47c511e73e3cd17bc1d55f5347d667
SuguruNanjiReddy <114736777+SuguruNanjiReddy@users.noreply.github.com>
2022-10-05 22:08:00 +0530

    Add files via upload

---
+++ java script day4/Assignment4(3).js
@@ -0,0 +1,15 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const even = array.filter((number) => {
+  return number % 2 === 0;
+});
+
+console.log(even);
+const array1=[1,2,3,4,5,6,7,8,9,10,15,17,19];
+const primeNumber=array1.filter(number=>{
+    for(let i=2;i<number;i++){
+        if(number%i===0)
+        return false;
+    }
+    return number!==1;
+});
+console.log(primeNumber);
